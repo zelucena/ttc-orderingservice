9f82d182f51f604c1ee5d2fcc7ee43f5df0b8a46
José Lucena <jhenrique@edu.univali.br>
2019-11-04 21:07:11 -0300

    Completar configuração

---
+++ scripts/mate-run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mate-terminal -t 0.node.bft -e ./node0.sh &
+sleep 5
+mate-terminal -t 1.node.bft -e ./node1.sh &
+sleep 5
+mate-terminal -t 2.node.bft -e ./node2.sh &
+sleep 5
+mate-terminal -t 3.node.bft -e ./node3.sh &
+
+mate-terminal -t 0.ibm.peer.bft -e ./peer0.sh &
+mate-terminal -t 1.ibm.peer.bft -e ./peer1.sh &
+mate-terminal -t 0.lasige.peer.bft -e ./peer2.sh &
+mate-terminal -t 1.lasige.peer.bft -e ./peer3.sh &
+
+mate-terminal -t 1000.frontend.bft -e ./frontend1000.sh &
+mate-terminal -t 1000.frontend.bft -e ./frontend2000.sh &
+
+mate-terminal -t 0.ibm.cli -e ./client0.sh &
+mate-terminal -t 0.lasige.cli -e ./client1.sh &
+mate-terminal -t 0.ibm.admin.cli -e ./admin0.sh
--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-gnome-terminal  -e ./node0.sh
+mate-terminal -t 0.node.bft -e ./node0.sh
 sleep 5
-gnome-terminal  -e ./node1.sh
+mate-terminal -t 1.node.bft -e ./node1.sh
 sleep 5
-gnome-terminal  -e ./node2.sh
+mate-terminal -t 2.node.bft -e ./node2.sh
 sleep 5
-gnome-terminal  -e ./node3.sh
+mate-terminal -t 3.node.bft -e ./node3.sh
 sleep 5
 
-gnome-terminal  -e ./peer0.sh
-gnome-terminal  -e ./peer1.sh
-gnome-terminal  -e ./peer2.sh
-gnome-terminal  -e ./peer3.sh
+mate-terminal -t 0.ibm.peer.bft -e ./peer0.sh
+mate-terminal -t 1.ibm.peer.bft -e ./peer1.sh
+mate-terminal -t 0.lasige.peer.bft -e ./peer2.sh
+mate-terminal -t 1.lasige.peer.bft -e ./peer3.sh
 
-gnome-terminal  -e ./frontend1000.sh 
-gnome-terminal  -e ./frontend2000.sh 
+mate-terminal -t 1000.frontend.bft -e ./frontend1000.sh 
+mate-terminal -t 1000.frontend.bft -e ./frontend2000.sh 
 
-gnome-terminal  -e ./client0.sh 
-gnome-terminal  -e ./client1.sh
+mate-terminal -t 0.ibm.cli -e ./client0.sh 
+mate-terminal -t 0.lasige.cli -e ./client1.sh
+mate-terminal -t 0.ibm.admin -e ./admin0.sh 
